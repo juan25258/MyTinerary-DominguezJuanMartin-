e704e58caaf1aa807a4812f6d1900c0e7c03a69c
juan25258 <juan25257@gmail.com>
2023-08-21 04:07:36 -0300

    ajuste un poco el responsive

--- src/pages/Cities/index.jsx
+++ src/pages/Cities/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Style from './Style.css'
+import { Link as LinkDetails } from 'react-router-dom'
 
 export default function Cities() {
   const [cities, setCities] = useState([]);
@@ -35,7 +36,7 @@ export default function Cities() {
           </div>
         </nav>
         <article className="City container">
-          <h2 className='d-flex ' >Cities</h2>
+          <h2 className='d-flex justify-content-center' >Cities</h2>
           <div className="card-deck d-flex flex-wrap justify-content-center gap-5">
             {cities.map(city => (
               <div className="card" key={city.id}>
@@ -43,7 +44,7 @@ export default function Cities() {
                 <div className="card-body">
                   <h5 className="card-title">{city.name}</h5>
                   <p className="card-text">{city.details}</p>
-                  <a href="#" className="btn btn-primary">Details</a>
+                  <a href="#" className="btn btn-primary"><LinkDetails to='/Details'>Details</LinkDetails> </a>
                 </div>
               </div>
             ))}
