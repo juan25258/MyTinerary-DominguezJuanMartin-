144dd15b4396e9fbc1100d87cd82e609e3ba7426
juan25258 <juan25257@gmail.com>
2023-09-13 23:46:15 -0300

    sign up diseño basico

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,17 +1,77 @@
-import React from 'react'
+import React from "react";
+import Style from "./Style.css";
 
 const SignUp = () => {
   return (
-    <div className="d-flex col-10 justify-center items-center">
-      <form
-        className="d-flex flex-column justify-center items-start gap-5 bg-gray-200 p-5"
-        //onSubmit={handleSubmit}
-      >
-        
-        <button className="btn btn-secondary"> Registrarse </button>
-      </form>
-    </div>
-  )
-}
+    <main className="sign-up">
+      <section id="register" className="row g-3">
+        <div className="row g-3">
+          <div className="col">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="First name"
+              aria-label="First name"
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+              aria-label="Last name"
+            />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <label for="inputEmail4" className="form-label">
+            Email
+          </label>
+          <input type="email" className="form-control" id="inputEmail4" />
+        </div>
 
-export default SignUp
+        <div classectione="col-md-4">
+          <label for="inputState" className="form-label">
+            Country
+          </label>
+          <select id="inputState" className="form-select">
+            <option selected>Choose...</option>
+            <option>...</option>
+            <option>...</option>
+            <option>...</option>
+          </select>
+        </div>
+        <div className="col-md-6">
+          <label for="inputPassword4" className="form-label">
+            Password
+          </label>
+          <input type="password" className="form-control" id="inputPassword4" />
+          <label for="inputPassword4" className="form-label">
+            Repeat Password
+          </label>
+          <input type="password" className="form-control" id="inputPassword4" />
+        </div>
+
+        <div className="col-12">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="gridCheck"
+            />
+            <label className="form-check-label" for="gridCheck">
+              I agree to the terms of registration
+            </label>
+          </div>
+        </div>
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default SignUp;
